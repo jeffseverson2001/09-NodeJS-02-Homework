d3fe7f53f3165fa5848410e667ee684ce90da72b
jeffseverson2001 <jeff@zealouscreations.com>
2021-06-12 19:32:37 -0500

    Night One Update

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter your Project Title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter the Description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter the Installation Instructions: ",
+        name: "instructions"
+    },
+    {
+        type: "input",
+        message: "Enter the Usage Information: ",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter the Contribution Guidelines: ",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        message: "Enter the Test Instructions: ",
+        name: "test"
+    },{
+        type: "list",
+        message: "What License do you want to use? ",
+        choices: [
+            "MIT",
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "ISC License"
+        ],
+        name: "license"
+    }
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("Readme File Created")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) =>
+        console.log(data)
+    )
+}
 
 // Function call to initialize app
 init();
